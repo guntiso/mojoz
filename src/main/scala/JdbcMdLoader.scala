edb5c1d471331e058db4b4e757fa07f29abd07ed
Guntis Ozols <guntiso@uniso.lv>
2014-04-08 02:46:31 +0300

    jdbc md loader

--- src/main/scala/JdbcMdLoader.scala
+++ src/main/scala/JdbcMdLoader.scala
@@ -1,10 +1,155 @@
 package mojoz.metadata.in
 
 import java.lang.Integer
+import java.sql.Connection
+import java.sql.{ DatabaseMetaData => DM }
+import java.sql.ResultSet
+import java.sql.{ ResultSet => RS }
 import java.sql.Types
 
-import mojoz.metadata._
+import scala.collection.mutable.ListBuffer
+
+import mojoz.metadata.ColumnDef
+import mojoz.metadata.DbIndex
+import mojoz.metadata.Ref
+import mojoz.metadata.TableDef
 import mojoz.metadata.{ XsdType => Type }
+
+object JdbcMdLoader {
+  def loadTableDefs(conn: Connection,
+    catalog: String, schemaPattern: String, tableNamePattern: String,
+    types: String*) = {
+    val tableDefs = ListBuffer[TableDef]()
+    val dmd = conn.getMetaData
+    val rs = dmd.getTables(catalog, schemaPattern, tableNamePattern,
+      if (types.size == 0) null else types.toArray)
+    while (rs.next) {
+      val catalog = rs.getString("TABLE_CAT")
+      val schema = rs.getString("TABLE_SCHEM")
+      val tableName = rs.getString("TABLE_NAME")
+      val comment = rs.getString("REMARKS")
+      val cols = loadColDefs(dmd.getColumns(catalog, schema, tableName, null))
+      val pk = loadPk(dmd.getPrimaryKeys(catalog, schema, tableName))
+      val uk = Nil // TODO uk
+      val idx = Nil // TODO idx
+      val refs = loadRefs(dmd.getImportedKeys(catalog, schema, tableName))
+      val tableFullName =
+        List(catalog, schema, tableName).filter(_ != null).mkString(".")
+      tableDefs += TableDef(tableFullName, comment, cols, pk, uk, idx, refs)
+    }
+
+    // work around oracle bugs
+    if (conn.getClass.getName.startsWith("oracle")) {
+      if (!tableDefs.exists(_.comment != null)) {
+        val st = conn.prepareStatement(
+          "select comments from all_tab_comments" +
+            " where owner || '.' || table_name = ?",
+          RS.TYPE_FORWARD_ONLY, RS.CONCUR_READ_ONLY, RS.CLOSE_CURSORS_AT_COMMIT)
+        tableDefs transform { td =>
+          st.setString(1, td.name)
+          val rs = st.executeQuery()
+          val comment = if (rs.next) rs.getString(1) else null
+          rs.close()
+          st.clearParameters()
+          if (comment == null) td else td.copy(comment = comment)
+        }
+      }
+      if (!tableDefs.exists(_.cols.exists(_.comment != null))) {
+        val st = conn.prepareStatement(
+          "select column_name, comments from all_col_comments" +
+            " where owner || '.' || table_name = ?",
+          RS.TYPE_FORWARD_ONLY, RS.CONCUR_READ_ONLY, RS.CLOSE_CURSORS_AT_COMMIT)
+        tableDefs transform { td =>
+          st.setString(1, td.name)
+          val rs = st.executeQuery()
+          var cList: List[(String, String)] = Nil
+          while (rs.next)
+            cList = (rs.getString(1), rs.getString(2)) :: cList
+          rs.close()
+          st.clearParameters()
+          val cMap = cList.toMap
+          if (!cMap.values.exists(c => c != null && c != "")) td
+          else td.copy(cols = td.cols.map(c =>
+            c.copy(comment = cMap.get(c.name).orNull)))
+        }
+      }
+    }
+    tableDefs.toList
+  }
+  def loadColDefs(rs: ResultSet) = {
+    val cols = ListBuffer[ColumnDef]()
+    while (rs.next) {
+      val name = rs.getString("COLUMN_NAME")
+      val jdbcTypeCode = rs.getInt("DATA_TYPE") // TODO use it?
+      val dbTypeName = rs.getString("TYPE_NAME")
+      val size = rs.getInt("COLUMN_SIZE")
+      val fractionDigits = rs.getInt("DECIMAL_DIGITS")
+      val nullable = rs.getInt("NULLABLE") == DM.columnNullable
+      val dbDefault = rs.getString("COLUMN_DEF")
+      val comment = rs.getString("REMARKS")
+      val check = null // TODO check constraint for column (enum)!
+      // TODO extract mapping, use db type!
+      val dbType = JdbcToXsdTypeMapper.map(jdbcTypeCode, size, fractionDigits)
+      cols += ColumnDef(name, dbType, nullable, dbDefault, check, comment)
+    }
+    rs.close
+    cols.toList
+  }
+  def loadPk(rs: ResultSet) = {
+    var cols: List[(Short, String, String)] = Nil
+    while (rs.next) {
+      val keySeq = rs.getShort("KEY_SEQ")
+      val colName = rs.getString("COLUMN_NAME")
+      val pkName = rs.getString("PK_NAME")
+      cols = (keySeq, colName, pkName) :: cols
+    }
+    rs.close
+    val pkName = cols.map(_._3).headOption.orNull
+    if (cols.size == 0) None else Some(DbIndex(pkName, cols.sorted.map(_._2)))
+  }
+  def loadRefs(rs: ResultSet) = {
+    def fkRule(rule: Short) = rule match {
+      case DM.importedKeyNoAction | DM.importedKeyRestrict => "no action"
+      case DM.importedKeyCascade => "cascade"
+      case DM.importedKeySetNull => "set null"
+      case DM.importedKeySetDefault => "set default"
+      case _ => sys.error("Unexpected fk rule: " + rule)
+    }
+    val refMap = scala.collection.mutable.Map[(String, String), Ref]()
+    while (rs.next) {
+      val pkTabCat = rs.getString("PKTABLE_CAT")
+      val pkTabSch = rs.getString("PKTABLE_SCHEM")
+      val pkTabName = rs.getString("PKTABLE_NAME")
+      val pkColName = rs.getString("PKCOLUMN_NAME")
+      val fkColName = rs.getString("FKCOLUMN_NAME")
+      val keySeq = rs.getShort("KEY_SEQ")
+      val updateRule = fkRule(rs.getShort("UPDATE_RULE"))
+      val deleteRule = fkRule(rs.getShort("DELETE_RULE"))
+      val fkName = rs.getString("FK_NAME")
+      val deferrability = rs.getShort("DEFERRABILITY") match {
+        case DM.importedKeyInitiallyDeferred => "deferred"
+        case DM.importedKeyInitiallyImmediate => "immediate"
+        case DM.importedKeyNotDeferrable => "not deferrable"
+      }
+      val pkTabFullName =
+        List(pkTabCat, pkTabSch, pkTabName).filter(_ != null).mkString(".")
+      refMap.get(pkTabFullName, fkName) match {
+        case Some(r) =>
+          r.cols.asInstanceOf[ListBuffer[String]] += fkColName
+          r.refCols.asInstanceOf[ListBuffer[String]] += pkColName
+        case None =>
+          // TODO deferrability to ref!
+          refMap += (pkTabFullName, fkName) ->
+            Ref(fkName, ListBuffer(fkColName), pkTabFullName,
+              ListBuffer(pkColName), null, null, deleteRule, updateRule)
+      }
+    }
+    rs.close
+    refMap.values
+      .map(r => r.copy(cols = r.cols.toList, refCols = r.refCols.toList))
+      .toList
+  }
+}
 
 // java.sun.com/j2se/1.5.0/docs/guide/jdbc/getstart/GettingStartedTOC.fm.html
 object JdbcToXsdTypeMapper {
