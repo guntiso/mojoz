26dadeb9f4cd35adb401e7502a73f8ffae91d758
Guntis Ozols <guntiso@uniso.lv>
2014-06-07 16:21:42 +0300

    cleanup

--- src/main/scala/XsdWriter.scala
+++ src/main/scala/XsdWriter.scala
@@ -48,7 +48,6 @@ class XsdWriter(metadata: Metadata[Type],
     val maxOccurs = Option(col.maxOccurs) getOrElse {
       if (col.isCollection) "unbounded" else null
     }
-    // FIXME for refed values, depends on ref-chain nullable!
     val minOccurs = if (required) null else "0"
     val nillable = if (required || col.isCollection) null else "true"
     val typeName =
