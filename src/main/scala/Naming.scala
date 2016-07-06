c796e83d71343f33bfa16773094d69dd2813d2c6
Guntis Ozols <guntiso@uniso.lv>
2016-07-06 07:44:08 +0300

    naming - legacy cleanup

--- src/main/scala/Naming.scala
+++ src/main/scala/Naming.scala
@@ -3,26 +3,19 @@ package mojoz.metadata
 object Naming {
   def dbNameToXsdName(dbName: String) = {
     val parts = dbName.split("[_\\.]+")
-    parts.toList.map(_.toLowerCase match {
-      case "usr" => "user"
-      case "grp" => "group"
-      case "rle" => "role"
-      case x => x
-    }).map(_.capitalize).mkString
+    parts.toList
+      .map(_.toLowerCase)
+      .map(_.capitalize)
+      .mkString
   }
-  def dbName(xsdName: String) = {
-    val parts = dasherize(xsdName).split("[\\-\\_]")
-    val hadPrefix = xsdName.startsWith("_")
-    val hadSuffix = xsdName.endsWith("_")
-    val clean = parts.toList.map(_.toLowerCase match {
-      case "user" => "usr"
-      case "group" => "grp"
-      case "role" => "rle"
-      case x => x
-    }).mkString("_").replace("__", "_")
-      .replace("_1", "1") // no underscore before 1 in our database names
-      .replace("_2", "2") // no underscore before 2 in our database names
-      .replace("_3", "3") // no underscore before 3 in our database names
+  def dbName(name: String) = {
+    val parts = dasherize(name).split("[\\-\\_]")
+    val hadPrefix = name.startsWith("_")
+    val hadSuffix = name.endsWith("_")
+    val clean = parts.toList
+      .map(_.toLowerCase)
+      .mkString("_")
+      .replace("__", "_")
     (hadPrefix, hadSuffix) match {
       case (true, true) => "_" + clean + "_"
       case (true, false) => "_" + clean
