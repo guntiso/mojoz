d8e840e9551418441bc4a64990b3175207e3c365
Guntis Ozols <guntiso@uniso.lv>
2018-02-19 22:28:57 +0200

    0.9-SNAPSHOT - FieldDef.default renamed to FieldDef.initial

--- src/main/scala/YamlViewDefLoader.scala
+++ src/main/scala/YamlViewDefLoader.scala
@@ -67,7 +67,7 @@ object FieldDef {
     val saveTo: String
     val resolver: String // expression, calculates value to be saved
     val nullable: Boolean
-    val default: String
+    val initial: String
     val type_ : T
     val enum: Seq[String]
     val joinToParent: String
@@ -88,7 +88,7 @@ case class FieldDef[+T](
   saveTo: String,
   resolver: String, // expression, calculates value to be saved
   nullable: Boolean,
-  default: String,
+  initial: String,
   isForcedCardinality: Boolean,
   type_ : T,
   enum: Seq[String],
@@ -110,7 +110,7 @@ case class FieldDef[+T](
     saveTo = null,
     resolver = null, // expression, calculates value to be saved
     nullable = true,
-    default = null,
+    initial = null,
     isForcedCardinality = false,
     type_  = type_,
     enum = null,
@@ -133,7 +133,7 @@ case class FieldDef[+T](
     saveTo = that.saveTo,
     resolver = that.resolver, // expression, calculates value to be saved
     nullable = that.nullable,
-    default = that.default,
+    initial = that.initial,
     isForcedCardinality = if (that.isInstanceOf[FieldDef[_]]) that.asInstanceOf[FieldDef[_]].isForcedCardinality else false,
     type_ = that.type_,
     enum = that.enum,
@@ -300,7 +300,7 @@ class YamlViewDefLoader(
       val resolver = yfd.resolver
       val nullable = Option(yfd.cardinality)
         .map(c => Set("?", "*").contains(c)) getOrElse true
-      val default = null // TODO fieldDef default?
+      val initial = null // TODO fieldDef initial?
       val isForcedCardinality = yfd.cardinality != null
       val joinToParent = yfd.joinToParent
       val enum = yfd.enum
@@ -324,7 +324,7 @@ class YamlViewDefLoader(
         if (xsdTypeFe != null) xsdTypeFe else rawXsdType getOrElse null
 
       FieldDef(table, tableAlias, name, alias, options, isCollection, maxOccurs,
-        isExpression, expression, saveTo, resolver, nullable, default, isForcedCardinality,
+        isExpression, expression, saveTo, resolver, nullable, initial, isForcedCardinality,
         xsdType, enum, joinToParent, orderBy, false, comment, extras)
     }
     def isViewDef(m: Map[String, Any]) =
