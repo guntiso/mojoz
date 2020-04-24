07e25ab76ba4c90f077be5375bb9e2c1bed33df5
Guntis Ozols <guntiso@uniso.lv>
2020-04-24 17:32:43 +0300

    mv-produced helper utility for test development

---
+++ mv-produced.cmd
@@ -0,0 +1,10 @@
+mv src/test/resources/case-classes-out-produced.scala     src/test/resources/case-classes-out.scala
+mv src/test/resources/classes-out-produced.scala          src/test/resources/classes-out.scala
+mv src/test/resources/tables-out-h2-produced.sql          src/test/resources/tables-out-h2.sql
+mv src/test/resources/tables-out-oracle-produced.sql      src/test/resources/tables-out-oracle.sql
+mv src/test/resources/tables-out-postgresql-produced.sql  src/test/resources/tables-out-postgresql.sql
+mv src/test/resources/tables-out-hsqldb-produced.sql      src/test/resources/tables-out-hsqldb.sql
+mv src/test/resources/tables-out-produced.yaml            src/test/resources/tables-out.yaml
+mv src/test/resources/views-out-produced.yaml             src/test/resources/views-out.yaml
+mv src/test/resources/xsd-bindings-out-produced.xjb       src/test/resources/xsd-bindings-out.xjb
+mv src/test/resources/xsd-out-produced.xsd                src/test/resources/xsd-out.xsd
