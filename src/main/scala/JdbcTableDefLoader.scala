c41fb8dd1921bcc754fe0105131b80e0c51a7471
Guntis Ozols <guntiso@uniso.lv>
2024-11-27 16:15:02 +0200

    jdbc tabledef loader resultset leak fix

--- src/main/scala/JdbcTableDefLoader.scala
+++ src/main/scala/JdbcTableDefLoader.scala
@@ -68,6 +68,7 @@ abstract class JdbcTableDefLoader(typeDefs: Seq[TypeDef]) {
       tableDefs += JdbcTableDef(db, tableFullName, comments, mappedCols,
           pk, uk, unmappedCk, idx, refs, extras)
     }
+    rs.close()
 
     // work around oracle bugs
     if (conn.getClass.getName.startsWith("oracle")) {
